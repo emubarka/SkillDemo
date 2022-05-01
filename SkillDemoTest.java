c6c466693440e7ca1f211e883c2b2ac8521a7639
emubarka <emubarka@yahoo.com>
2022-05-01 14:17:08 -0700

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void subtract() {
-        assertEquals(2, SkillDemo.subtract(4,3));
+        assertEquals(2, SkillDemo.subtract(4,2));
     }
 }
